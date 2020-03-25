4da0995ea9ab33ab84b494d5acf705696b27e5ed
Masha Enot <masha.p.simeonova@gmail.com>
2020-03-25 12:12:09 +0200

    Test commit

---
+++ 9 and 10 March/01Basketball Equipment/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _01Basketball_Equipment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
